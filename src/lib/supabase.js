968a4133fdec37ce2f32139eac6895b8f69437b8
erickdrksca <erickdrksca@yahoo.com>
2026-06-02 10:57:52 -0600

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,11 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://xyyelqitwyxuoxsjkuap.supabase.co/rest/v1/"; 
-const supabaseAnonKey = "sb_publishable_iqwqKy4lgwMf4fwC7dpXDQ_8Z7RvkXm"; 
+const supabaseUrl = "https://xyyelqitwyxuoxsjkuap.supabase.co";
+
+const supabaseAnonKey = "TU_KEY";
+
 export const supabase = createClient(
-
   supabaseUrl,
-
   supabaseAnonKey
-
 );
